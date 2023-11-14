59f8a60ee41500d08141f9cf72a3ef4153d5a416
KatyshaVtyrina <katyshavtyrina@gmail.com>
2023-11-14 20:39:49 +0300

    create Course, Lesson

--- education/models.py
+++ education/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
-# Create your models here.
+
+NULLABLE = {'blank': True, 'null': True}
+
+
+class Course(models.Model):
+    title = models.CharField(max_length=150, verbose_name='название')
+    preview = models.ImageField(verbose_name='картинка', **NULLABLE)
+    description = models.TextField(verbose_name='описание', **NULLABLE)
+
+    def __str__(self):
+        return f'Название курса - {self.title}'
+
+    class Meta:
+        verbose_name = 'курс'
+        verbose_name_plural = 'курсы'
+
+
+class Lesson(models.Model):
+    title = models.CharField(max_length=150, verbose_name='название')
+    preview = models.ImageField(verbose_name='картинка', **NULLABLE)
+    description = models.TextField(verbose_name='описание', **NULLABLE)
+    url = models.URLField(verbose_name='ссылка на видео', **NULLABLE)
+
+    def __str__(self):
+        return f'Название урока - {self.title}'
+
+    class Meta:
+        verbose_name = 'урок'
+        verbose_name_plural = 'уроки'
