483dc4be01fbf756196e90d09afcbac62082a31b
KatyshaVtyrina <katyshavtyrina@gmail.com>
2023-11-16 07:09:13 +0300

    add course in Lesson

--- education/models.py
+++ education/models.py
@@ -9,6 +9,7 @@ class Course(models.Model):
     preview = models.ImageField(verbose_name='картинка', **NULLABLE)
     description = models.TextField(verbose_name='описание', **NULLABLE)
 
+
     def __str__(self):
         return f'Название курса - {self.title}'
 
@@ -22,6 +23,7 @@ class Lesson(models.Model):
     preview = models.ImageField(verbose_name='картинка', **NULLABLE)
     description = models.TextField(verbose_name='описание', **NULLABLE)
     url = models.URLField(verbose_name='ссылка на видео', **NULLABLE)
+    course = models.ForeignKey(Course, on_delete=models.CASCADE, verbose_name='курс', **NULLABLE)
 
     def __str__(self):
         return f'Название урока - {self.title}'
