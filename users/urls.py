69fc32e6a71d9b64a7a5b5c08aa2655df41e45d9
KatyshaVtyrina <katyshavtyrina@gmail.com>
2023-11-17 07:12:38 +0300

    add ViewSetUser and create superuser

--- users/urls.py
+++ users/urls.py
@@ -3,14 +3,12 @@ from rest_framework.routers import DefaultRouter
 from users.apps import UsersConfig
 from users.views import UserViewSet
 
-app_name = UsersConfig
+app_name = UsersConfig.name
 
 router = DefaultRouter()
 router.register(r'users', UserViewSet, basename='users')
 
 
-urlpatterns = [
-    # path('user/register/', UserRegisterAPIView.as_view(), name='register'),
-    # path('user/update/<int:pk>/', UserProfileAPIView.as_view(), name='user_profile'),
-    # path('user/delete/<int:pk>/', UserDestroyAPIView.as_view(), name='user_delete'),
-] + router.urls
+urlpatterns = (
+        [
+] + router.urls)
