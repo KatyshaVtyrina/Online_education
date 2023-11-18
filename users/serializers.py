0ccae5421efb5b9fc059b356da1ef0aab09fe04a
KatyshaVtyrina <katyshavtyrina@gmail.com>
2023-11-18 08:26:51 +0300

    add payments in UserSerializer

--- users/serializers.py
+++ users/serializers.py
@@ -1,10 +1,13 @@
 from rest_framework import serializers
 
+from education.serializers import PaymentSerializer
 from users.models import User
 
 
 class UserSerializer(serializers.ModelSerializer):
     """Класс-сериализатор для модели User"""
+    payments = PaymentSerializer(source='payments_set.all', many=True)
+
     class Meta:
         model = User
         fields = '__all__'
