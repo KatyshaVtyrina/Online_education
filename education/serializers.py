ac8eab66173c0459b1e470b4e29d0eeb8d063caa
KatyshaVtyrina <katyshavtyrina@gmail.com>
2023-11-24 17:15:29 +0300

    correction fields

--- education/serializers.py
+++ education/serializers.py
@@ -1,9 +1,14 @@
 from rest_framework import serializers
+from rest_framework.relations import SlugRelatedField
 
 from education.models import Course, Lesson, Payments
+from users.models import User
 
 
 class LessonSerializer(serializers.ModelSerializer):
+    course = SlugRelatedField(slug_field='title', queryset=Course.objects.all())
+    owner = SlugRelatedField(slug_field='email', queryset=User.objects.all())
+
     """Класс-сериализатор для модели Lesson"""
     class Meta:
         model = Lesson
@@ -15,6 +20,7 @@ class CourseSerializer(serializers.ModelSerializer):
     # количество уроков у курса
     count_lessons = serializers.IntegerField(source='lesson_set.all.count', read_only=True)
     lessons = LessonSerializer(source='lesson_set', many=True, read_only=True)
+    owner = SlugRelatedField(slug_field='email', queryset=User.objects.all())
 
     class Meta:
         model = Course
@@ -23,10 +29,12 @@ class CourseSerializer(serializers.ModelSerializer):
 
 class PaymentSerializer(serializers.ModelSerializer):
     """Класс-сериализатор для модели Payment"""
+    course = SlugRelatedField(slug_field='title', queryset=Course.objects.all())
+    lesson = SlugRelatedField(slug_field='title', queryset=Lesson.objects.all())
 
     class Meta:
         model = Payments
-        fields = '__all__'
+        fields = ('id', 'date_of_payment', 'amount', 'payment_method', 'course', 'lesson')
 
 
 class LessonListSerializer(serializers.ModelSerializer):
