266b7dc0cfa782b396431652a1ef42b0d2bcfbc6
KatyshaVtyrina <katyshavtyrina@gmail.com>
2023-11-26 01:42:06 +0300

    add NULLABLE in user for Subscription

--- education/models.py
+++ education/models.py
@@ -58,7 +58,7 @@ class Payments(models.Model):
 
 class Subscription(models.Model):
     course = models.ForeignKey(Course, on_delete=models.CASCADE, related_name='subscription', verbose_name='курс')
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, verbose_name='пользователь')
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, verbose_name='пользователь', **NULLABLE)
     is_subscription = models.BooleanField(default=False, verbose_name='признак подписки')
 
     def __str__(self):
