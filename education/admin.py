276de941702a79273a287681b1491777bdf3e342
KatyshaVtyrina <katyshavtyrina@gmail.com>
2023-11-26 01:42:16 +0300

    add admin

--- education/admin.py
+++ education/admin.py
@@ -1,3 +1,31 @@
 from django.contrib import admin
 
-# Register your models here.
+from education.models import Subscription, Course, Lesson, Payments
+
+
+@admin.register(Course)
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ('title', 'preview', 'description', 'owner')
+    search_fields = ('title',)
+    list_filter = ('title',)
+
+
+@admin.register(Lesson)
+class LessonAdmin(admin.ModelAdmin):
+    list_display = ('title', 'preview', 'description', 'url', 'course', 'owner')
+    search_fields = ('title',)
+    list_filter = ('title',)
+
+
+@admin.register(Payments)
+class PaymentsAdmin(admin.ModelAdmin):
+    list_display = ('user', 'date_of_payment', 'course', 'lesson', 'amount', 'payment_method')
+    search_fields = ('user',)
+    list_filter = ('user', 'date_of_payment')
+
+
+@admin.register(Subscription)
+class SubscriptionAdmin(admin.ModelAdmin):
+    list_display = ('course', 'user', 'is_subscription')
+    search_fields = ('course', 'user')
+    list_filter = ('user', 'course')
