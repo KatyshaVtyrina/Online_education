aacad7df3c4ebfb364b29db25a46932a649e89e5
KatyshaVtyrina <katyshavtyrina@gmail.com>
2023-12-01 06:49:23 +0300

    correction

--- config/urls.py
+++ config/urls.py
@@ -38,6 +38,6 @@ urlpatterns = [
     path('', include('education.urls', namespace='education')),
     path('', include('users.urls', namespace='users')),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-   path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 
 ]
