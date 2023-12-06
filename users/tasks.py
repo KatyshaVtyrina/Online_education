7ec229f5bef19e68ea2c8005a00b2b52d2db644d
KatyshaVtyrina <katyshavtyrina@gmail.com>
2023-12-06 07:56:34 +0300

    add '()' for shared_task

--- users/tasks.py
+++ users/tasks.py
@@ -6,7 +6,7 @@ from django.utils import timezone
 from users.models import User
 
 
-@shared_task
+@shared_task()
 def change_status_user():
     users = User.objects.all().filter(is_active=True)
     for user in users:
